91d088f1c71bcffb5abfb48a8fa2d7aa7c7b80c2
NghiemHong <Hung24855@gmail.com>
2023-11-27 13:45:50 +0700

    Dựng khung layout mặc định

--- src/App.js
+++ src/App.js
@@ -7,29 +7,30 @@ import DefaultLayout from "./components/Layout/DefaultLayout";
 function App() {
   return (
     <Router>
-      <div className="App"></div>
-      <Routes>
-        {publicRoutes.map((route, index) => {
-          let Layout = DefaultLayout;
-          if (route.layout) {
-            Layout = route.layout;
-          } else if (route.layout === null) {
-            Layout = Fragment;
-          }
-          const Page = route.component;
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            ></Route>
-          );
-        })}
-      </Routes>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              ></Route>
+            );
+          })}
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,5 +1,10 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss";
+
+const cx = classNames.bind(styles);
+
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return <aside className={cx("wrapper")}>Sidebar</aside>;
 };
 
 export default SideBar;
